a07990a9b24faeb29c01fee2daaf5193e89adb23
Felipe Rivas <rivascf@gmail.com>
2024-11-21 11:11:08 -0600

    Updated test scripts and oled script.

--- dofbot_arm/src/yahboom_arm_lib/test_led_rgb.py
+++ dofbot_arm/src/yahboom_arm_lib/test_led_rgb.py
@@ -9,11 +9,11 @@ time.sleep(.1)
 def main():
     
     while True:
-        Arm.Arm_RGB_set(50, 0, 0) #RGB亮红灯  RGB red
+        Arm.Arm_RGB_set(50, 0, 0) # RGB red
         time.sleep(.5)
-        Arm.Arm_RGB_set(0, 50, 0) #RGB亮绿灯  RGB green
+        Arm.Arm_RGB_set(0, 50, 0) # RGB green
         time.sleep(.5)
-        Arm.Arm_RGB_set(0, 0, 50) #RGB亮蓝灯  RGB blue
+        Arm.Arm_RGB_set(0, 0, 50) # RGB blue
         time.sleep(.5)
 
         print(" END OF LINE! ")
