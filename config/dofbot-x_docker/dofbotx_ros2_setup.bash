d2adba75e985f4e3aa3d6d9e6f015f7d0981d963
Felipe Rivas <52331791+rivascf@users.noreply.github.com>
2024-12-03 11:34:35 -0600

    Update dofbotx_ros2_setup.bash

--- config/dofbot-x_docker/dofbotx_ros2_setup.bash
+++ config/dofbot-x_docker/dofbotx_ros2_setup.bash
@@ -1,7 +1,3 @@
-# Setting ROS2 DOMAIN to ARRG channel (default value)
-export ROS_DOMAIN_ID=10
-# Source DofbotX ROS2 Wrokspace
-source /home/Dofbot/ws_dofbot-x/install/setup.bash
 # Alias declarations
 alias srcthis='source /home/Dofbot/ws_dofbot-x/install/setup.bash'
 alias ros2path='sed "s/:/\n/g" <<< "$AMENT_PREFIX_PATH" | sort | uniq'
